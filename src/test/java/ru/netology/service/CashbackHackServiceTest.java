2ff01698d9bf6c9e410a58659de81c821c30ce58
Azize Zinadinova <azizeshka87@gmail.ru>
2021-12-15 16:39:23 +0300

    тесты с использованием API JUnit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,11 +3,13 @@ package ru.netology.service;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CashbackHackServiceTest {
     CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
-    public void shouldReturn100() {
+    public void shouldReturn100JUnitVintage() {
         int actual = cashbackHackService.remain(900);
         int expected = 100;
 
@@ -15,11 +17,28 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldReturnZero() {
+    public void shouldReturnZeroJUnitVintage() {
         int actual = cashbackHackService.remain(1000);
         int expected = 0;
 
         Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReturn100JUnitJupiter() {
+        int actual = cashbackHackService.remain(900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnZeroJUnitJupiter() {
+        int actual = cashbackHackService.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
 
     }
 }
